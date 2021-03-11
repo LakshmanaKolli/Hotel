40c3ab58eaa16ad5fa6a221ae7bb7c870bd593b1
Setup <Setup@EPINHYDW07CA.princeton.epam.com>
2021-03-11 13:19:37 +0530

    modified groovy contract

--- src/test/resources/contracts/reservation_gethotel_details.groovy
+++ src/test/resources/contracts/reservation_gethotel_details.groovy
@@ -2,7 +2,7 @@ import org.springframework.cloud.contract.spec.Contract
 Contract.make{
 	request{
 		method 'GET'
-		urlPath('/hotels/api/v1/hotelDetails/1')
+		urlPath('/hotels/api/v1/hotelDetails/2')
 		headers {
 			contentType(applicationJson())
 		}
@@ -10,11 +10,23 @@ Contract.make{
 	response{
 		status OK()
 		body(
-		"hotel_Id":1,
-		"name":"AMB",
-		"description":"near ASP",
-		"rating":"4.5"
-		)
+				"hotel_Id":1,
+				"name":"AMB",
+				"description":"near ASP",
+				"rating":"4.5",
+				"phoneNumber":"123",
+				"room":["room_id":1,
+					"room_number":1,
+					"room_rate":1,
+					"room_availability":"Y",
+					"type":"D"],
+				"address":["id":1,
+					"country":"IN",
+					"state":"AP",
+					"city":"VZM",
+					"area":"BRP",
+					"pincode":"123"]
+				)
 		headers {
 			contentType(applicationJson())
 		}
